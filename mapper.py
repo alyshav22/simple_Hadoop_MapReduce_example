b962bfed7baa9905ef41cb48ede0d7a6727cd55f
alyshav22 <alyshav805@gmail.com>
2019-05-17 04:46:31 +0000

    lowecase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
-import sys
+import os.path
+from sklearn.feature_extraction import stop_words
+import string
 
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
+
+
+f = open(os.path.expanduser('~') + '/shakespeare.txt', 'r')
+outfile = os.path.expanduser('~') + '/intermediate_result'
+g = open(outfile, 'w')
+g.close()
 # get all lines from stdin
-for line in sys.stdin:
+for line in f:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+    line = line.translate(str.maketrans('', '', string.punctuation))
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+            #print(f'{word}\t1')
+            g=open(outfile, 'a')
+            g.write(f'{word}\t1\n')
+            g.close()
+
+f.close()
+
--- reducer.py
+++ reducer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import sys
+import os.path
 
+f = open(os.path.expanduser('~') + '/intermediate_result', 'r')
+outfile = os.path.expanduser('~') + '/final_result'
+g = open(outfile, 'w')
+g.close()
 # maps words to their counts
 word2count = {}
 
 # input comes from STDIN
-for line in sys.stdin:
+for line in f:
     # remove leading and trailing whitespace
     line = line.strip()
 
@@ -25,4 +30,10 @@ for line in sys.stdin:
 # write the tuples to stdout
 # Note: they are unsorted
 for word in word2count.keys():
-    print '%s\t%s'% ( word, word2count[word] )
+    #print(f'{word}\t{word2count[word]}')
+    #print '%s\t%s'% ( word, word2count[word] )
+    g=open(outfile, 'a')
+    g.write(f'{word}\t{word2count[word]}\n')
+    g.close()
+
+f.close()
